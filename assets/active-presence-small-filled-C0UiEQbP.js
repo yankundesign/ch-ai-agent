29e2470be195a4303a8964c25c96d601c52dc671
yankundesign <yankunux@gmail.com>
2025-12-10 09:59:37 -0800

    Updates

---
+++ assets/active-presence-small-filled-C0UiEQbP.js
@@ -0,0 +1,1 @@
+import{x as t}from"./index-DRbNw2Ie.js";const e=()=>t`<svg aria-hidden="true" part="icon" data-name="active-presence-small-filled" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14"><path d="M13 7A6 6 0 1 1 1 7a6 6 0 0 1 12 0"/></svg>`;export{e as default};
