3a06a0c87e6300362395249ee4158ce02ef59e4e
yankundesign <yankunux@gmail.com>
2025-12-18 14:38:39 -0800

    Updates

---
+++ assets/shape-circle-sm-filled-DY2H-s_e.js
@@ -0,0 +1,1 @@
+import{x as t}from"./index-CDY5yB2M.js";const e=()=>t`<svg aria-hidden="true" part="icon" data-name="shape-circle-sm-filled" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"><path d="M16 11.5a4.5 4.5 0 1 0 0 9 4.5 4.5 0 0 0 0-9"/></svg>`;export{e as default};
